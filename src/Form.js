26c1ffc4cb261640be6194059e1f8e622e5c2cc0
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2019-08-08 16:36:03 -0400

    resize modal handler and close button

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SignUp } from "./components/signup/signup";
 import styles from "./css/_index.scss";
 import { Login } from "./components/login/login";
 import Modal from "react-modal";
 import modalStyle from "./components/modal/_modal.scss";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Form = React.forwardRef((props, ref) => {
   const [appType, setAppType] = useState("");
   const [modal, modalState] = useState(false);
+
+  const loginRef = React.createRef();
+  const createRef = React.createRef();
 
   const setModalState = () => {
     if (!modal) modalState(true);
@@ -18,12 +23,33 @@ const Form = React.forwardRef((props, ref) => {
     setAppType(type);
   };
 
+  useEffect(() => {
+    handleResizeModal();
+  }, [appType]);
+
   window.PMApp = setAppType;
   window.PMSignInModal = setModalState;
 
+  const handleResizeModal = () => {
+    setTimeout(() => {
+      document.querySelectorAll(`.${modalStyle.modalOpen}`).forEach(modal => {
+        modal.removeAttribute("style");
+        if (window.innerHeight < modal.clientHeight) {
+          modal.style.height = `${window.innerHeight - 40}px`;
+        } else if (window.innerHeight > modal.clientHeight + 40) {
+          modal.removeAttribute("style");
+        }
+      });
+    }, 20);
+  };
+
+  window.addEventListener("resize", handleResizeModal);
+
   if (appType === "login") {
+    window.dispatchEvent(new Event("resize"));
     return (
       <Modal
+        ref={loginRef}
         ariaHideApp={false}
         isOpen={modal}
         onRequestClose={setModalState}
@@ -39,11 +65,16 @@ const Form = React.forwardRef((props, ref) => {
         closeTimeoutMS={200}
       >
         <Login appType={appType} modal={currentModal} />
+        <button onClick={setModalState} className={modalStyle.modalCloseIcon}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
       </Modal>
     );
   } else if (appType === "create") {
+    window.dispatchEvent(new Event("resize"));
     return (
       <Modal
+        ref={createRef}
         ariaHideApp={false}
         isOpen={modal}
         onRequestClose={setModalState}
@@ -59,6 +90,9 @@ const Form = React.forwardRef((props, ref) => {
         closeTimeoutMS={200}
       >
         <SignUp appType={appType} modal={currentModal} />
+        <button onClick={setModalState} className={modalStyle.modalCloseIcon}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
       </Modal>
       //TODO Create "x" icon for close modal
     );
