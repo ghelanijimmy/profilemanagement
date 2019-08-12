64aa61c19067801b6650a341be04b36268776f1f
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2019-08-12 15:46:34 -0400

    refactored homesearch.js to render minimal things

--- src/components/recentsearch/homesearch.js
+++ src/components/recentsearch/homesearch.js
@@ -19,6 +19,12 @@ const HomeSearch = props => {
     changeSearchOption(searchOption === "search" ? "list" : "search");
   };
 
+  let hasRecentSearches = true;
+  let hasFavouriteList = true;
+
+  hasRecentSearches = true;
+  hasFavouriteList = true;
+
   return (
     <section className={searchStyles.recentSearchesWrapper}>
       <div className={searchStyles.header}>
@@ -50,17 +56,22 @@ const HomeSearch = props => {
           </button>
         </div>
       </div>
-      <div className={searchStyles.searchContentWrapper}>
-        <SearchboxCard
-          headingIcon={faPlane}
-          headingText={"Flight"}
-          titleText={"Montreal-Montago Bay"}
-          legText={"One way | Aug 25 | 2 Adults, 2 Children"}
-        />
-        <SearchboxCard
-          hasBgImg={true}
-          titleText={"Montreal-Montago Bay"}
-          cardContent={`<p class=${searchStyles.title}>Montreal-Montago Bay</p>
+      {searchOption === "search" ? (
+        hasRecentSearches === true ? (
+          <React.Fragment>
+            <div className={searchStyles.searchContentWrapper}>
+              <SearchboxCard
+                headingIcon={faPlane}
+                headingText={"Flight"}
+                titleText={"Montreal-Montago Bay"}
+                legText={"One way | Aug 25 | 2 Adults, 2 Children"}
+              />
+              <SearchboxCard
+                hasBgImg={true}
+                titleText={"Montreal-Montago Bay"}
+                cardContent={`<p class=${
+                  searchStyles.title
+                }>Montreal-Montago Bay</p>
                   <p>Punta Cana, Dominican Republic</p>
                   <p>Superior 4.2/5 (1000+ reviews)</p>
                   <p>All inclusive | 5 days | Aug 25-Aug 30</p>
@@ -68,21 +79,59 @@ const HomeSearch = props => {
                   <p class=${
                     searchStyles.searchCardFooterText
                   }>15+ room types</p>`}
-        />
-      </div>
-      <div className={searchStyles.footer}>
-        <p>Prices vary and will be updated when you view current results.</p>
-        <p>
-          <span className={searchStyles.emailSeeAll}>
-            <a>
-              <FontAwesomeIcon icon={faEnvelope} />
-              Email my searches
-            </a>
-            <span>|</span>
-            <a>View all</a>
-          </span>
-        </p>
-      </div>
+              />
+            </div>
+
+            <div className={searchStyles.footer}>
+              <p>
+                Prices vary and will be updated when you view current results.
+              </p>
+              <p>
+                <span className={searchStyles.emailSeeAll}>
+                  <a>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    Email my searches
+                  </a>
+                  <span>|</span>
+                  <a>View all</a>
+                </span>
+              </p>
+            </div>
+          </React.Fragment>
+        ) : null
+      ) : hasFavouriteList === true ? null : (
+        <div
+          className={`${searchStyles.searchContentWrapper} ${
+            searchStyles.emptyFavouriteList
+          }`}
+        >
+          <div>
+            <p>
+              Saving your favourite trips, flights and hotels helps you keep eye
+              on the deal and saves your time when you are ready to book.
+            </p>
+          </div>
+          <div>
+            <p>
+              While looking for the deals...
+              <br />
+              Look for <FontAwesomeIcon icon={faHeart} /> to save your favourite
+              trips and hotels.
+            </p>
+            <br />
+            <p>Get access to your favourite list anytime, anywhere.</p>
+            <p>
+              <span>
+                <a>Sign In</a>
+              </span>{" "}
+              or{" "}
+              <span>
+                <a>Create Profile</a>
+              </span>
+            </p>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
