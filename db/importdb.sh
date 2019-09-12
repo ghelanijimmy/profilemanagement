a1e5418e13fad3be1d21461b79abf1c467221547
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2019-09-12 16:16:03 -0400

    docker containerize mern stack app

---
+++ db/importdb.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#mongoimport --host mongodb:27017 --db accountprofile --collection users --type json --file /backup.json
+#
+#mongo --quiet --eval  "printjson(db.adminCommand('listDatabases'))"
+
+cd /
+
+ls
